bcd86bca1f78857e2016028c9d81a2342a55a576
narcisoleedev <narcisolee2002@gmail.com>
2024-06-10 19:41:04 -0300

    configuring heroku-postgres

--- src/database/config.js
+++ src/database/config.js
@@ -3,13 +3,12 @@ require("dotenv").config();
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  host: process.env.HOST,
-  port: process.env.DBPORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+   idleTimeoutMillis: 30000,
+   connectionTimeoutMillis: 2000,
 });
 
 module.exports = pool;
