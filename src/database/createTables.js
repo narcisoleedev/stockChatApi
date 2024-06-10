1caa0bcba9551a927f662c70588bd6fa66004300
narcisoleedev <narcisolee2002@gmail.com>
2024-06-10 20:03:25 -0300

    configurating heroku pt5

---
+++ src/database/createTables.js
@@ -0,0 +1,13 @@
+const pool = require('./config.js')
+
+const client = await pool.connect();
+const response = await client.query(
+    `CREATE TABLE usersTable(
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(50) UNIQUE NOT NULL,
+        cpf VARCHAR(50) PRIMARY KEY NOT NULL,
+        password VARCHAR(100) NOT NULL
+    );`
+);
+client.release();
+console.log(response);
